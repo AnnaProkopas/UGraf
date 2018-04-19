828f13e0736f5ac2046c41e239391c4a68bfc4bf
AnnaProkopas <anna_prokopas@mail.ru>
2018-04-19 10:35:17 +1000

    add open and save dialog

--- mainwindow.h
+++ mainwindow.h
@@ -11,9 +11,9 @@
 #include <QGuiApplication>
 #include <QImage>
 
-/*class QAction;
+class QAction;
 class QMenu;
-class QPlainTextEdit;
+/*class QPlainTextEdit;
 class QSessionManager;*/
 class RenderArea;
 
@@ -25,33 +25,22 @@ public:
      MainWindow();
     ~MainWindow();
      void read();
-/*     void loadFile(const QString &fileName);
-protected:
-     void closeEvent(QCloseEvent *event) override;
-private slots:
-     void newFile();
-     void open();
-     bool save();
-     bool saveAs();
-     void about();
-     void documentWasModified();
-#ifndef QT_NO_SESSIONMANAGER
-     void commitData(QSessionManager &);
-#endif
 private:
-     void createActions();
-     void createStatusBar();
-     void readSettings();
-     void writeSettings();
-     bool maybeSave();
-     bool saveFile(const QString &fileName);
-     void setCurrentFile(const QString &fileName);
-     QString strippedName(const QString &fullFileName);
-     QPlainTextEdit *textEdit;
-     QString curFile;*/
-private:
+     QMenu *fileMenu;
      RenderArea *renderArea;
      QImage image;
+     QAction *openAct;
+     QAction *saveAct;
+     //QLabel *infoLabel;
+
+     void open();
+     void save();
+     void createActions();
+protected:
+#ifndef QT_NO_CONTEXTMENU
+    void contextMenuEvent(QContextMenuEvent *event) override;
+#endif // QT_NO_CONTEXTMENU
+     bool keypres(QKeyEvent *keyevent);
 };
 
 #endif // MAINWINDOW_H
